096a7a33948fa6d7805b81875345946e360c4990
youssefbourais0310 <youssef.bourais0310@gmail.com>
2021-12-25 18:33:23 +0100

    add some comment

--- checkmypass.py
+++ checkmypass.py
@@ -1,7 +1,7 @@
 import requests
 import hashlib
 import sys
-
+# updating something
 def request_api_data(query_char) :
     url = "https://api.pwnedpasswords.com/range/" + query_char
     res = requests.get(url)
